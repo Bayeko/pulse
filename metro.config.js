48923a23e27a70e9c58c985314dfc779945749bb
Bayeko <messadene.amir@gmail.com>
2025-08-06 16:30:29 +0200

    feat: add metro config alias

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+import { getDefaultConfig } from 'expo/metro-config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  '@': path.resolve(__dirname, 'src'),
+};
+
+export default config;
